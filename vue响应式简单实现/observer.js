93da0da25eb42c51a7d387eb1c58db90b13d0c73
前端-谢尚柱 <xieshangzhu@imechos.com>
2022-06-12 11:04:20 +0800

    :beers: 添加响应式对于数组的支持

--- vue响应式简单实现/observer.js
+++ vue响应式简单实现/observer.js
@@ -1,3 +1,12 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var data = {
     name: "如意虎头鞋",
     age: 18,
@@ -6,12 +15,29 @@ var data = {
         hobby: {
             specific: 'football'
         }
-    }
+    },
+    family: ['xue', 'hao', 'ting', 'li']
 };
 var observer = function (target) {
     // 针对非对象类型值， 直接返回该值
     if (typeof target !== 'object' || target === null)
         return target;
+    if (Array.isArray(target)) {
+        var oldArrayPrototype_1 = Array.prototype;
+        var convertArray_1 = Object.create(oldArrayPrototype_1);
+        ['push', 'pop', 'shift', 'unshift', 'splice'].forEach(function (method) {
+            convertArray_1[method] = function () {
+                var _a;
+                // 调用老方法
+                // @ts-ignore
+                (_a = oldArrayPrototype_1[method]).call.apply(_a, __spreadArray([this], arguments, false));
+                console.log("更新视图");
+            };
+        });
+        // 修改target原型
+        // @ts-ignore
+        target.__proto__ = convertArray_1;
+    }
     // 针对对象类型， 一次对属性做响应式处理
     for (var targetKey in target) {
         defineProperty(target, targetKey, target[targetKey]);
@@ -32,6 +58,6 @@ var defineProperty = function (target, key, value) {
     });
 };
 observer(data);
-data.info.gender = "female";
-data.info.hobby.specific = "basketball";
+// data.info.hobby.specific = "basketball"
+data.family.push('mama', 'baba');
 console.log(data);
--- vue响应式简单实现/observer.ts
+++ vue响应式简单实现/observer.ts
@@ -6,7 +6,8 @@ const data = {
     hobby: {
       specific: 'football'
     }
-  }
+  },
+  family: ['xue', 'hao', 'ting', 'li']
 }
 
 type ObserverData = Record<string, any>
@@ -14,6 +15,25 @@ type ObserverData = Record<string, any>
 const observer = (target: ObserverData) => {
   // 针对非对象类型值， 直接返回该值
   if (typeof target !== 'object' || target === null) return target
+
+
+  if (Array.isArray(target)) {
+    const oldArrayPrototype = Array.prototype;
+    const convertArray = Object.create(oldArrayPrototype);
+
+    ['push', 'pop', 'shift', 'unshift', 'splice'].forEach(method => {
+      convertArray[method] = function () {
+        // 调用老方法
+        // @ts-ignore
+        oldArrayPrototype[method].call(this, ...arguments)
+        console.log("更新视图")
+      }
+    })
+
+    // 修改target原型
+    // @ts-ignore
+    target.__proto__ = convertArray
+  }
 
   // 针对对象类型， 一次对属性做响应式处理
   for (const targetKey in target) {
@@ -40,7 +60,7 @@ const defineProperty = (target: ObserverData, key: string, value: any) => {
 
 
 observer(data)
-data.info.gender = "female"
-data.info.hobby.specific = "basketball"
+// data.info.hobby.specific = "basketball"
+data.family.push('mama', 'baba')
 console.log(data)
 
